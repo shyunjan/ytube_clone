b58c3a41a8342e485c26f307463548fbbb993321
Shin HyunJae <shyunjan@naver.com>
2025-07-23 15:40:44 +0900

    add and set mode-watcher for dark theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-	import '../app.css';
+  import '../app.css';
+  import SunIcon from '@lucide/svelte/icons/sun';
+  import MoonIcon from '@lucide/svelte/icons/moon';
+  import { ModeWatcher, toggleMode } from 'mode-watcher';
+  import { Button } from '$lib/components/ui/button';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
+
+<ModeWatcher track={false} defaultMode={'dark'} />
+<Button onclick={toggleMode} variant="outline" size="icon">
+  <SunIcon
+    class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90" />
+  <MoonIcon
+    class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0" />
+  <span class="sr-only">Toggle theme</span>
+</Button>
 
 {@render children()}
