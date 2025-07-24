b767ff420b8dfe8dd18ba07e14b4a9c6096b1427
Shin HyunJae <shyunjan@naver.com>
2025-07-24 14:18:45 +0900

    add Switch and Label component. and fix setting theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,50 @@
   import '../app.css';
   import SunIcon from '@lucide/svelte/icons/sun';
   import MoonIcon from '@lucide/svelte/icons/moon';
-  import { ModeWatcher, toggleMode } from 'mode-watcher';
-  import { Button } from '$lib/components/ui/button';
+  import { ModeWatcher, setMode } from 'mode-watcher';
+  import { Switch } from '$lib/components/ui/switch';
+  import { Label } from '$lib/components/ui/label';
 
   let { children } = $props();
+  let themeLight = $state(false);
+
+  setMode('dark');
+
+  // $effect(() => {
+  //   tick().then(() => {
+  //     console.debug(`changing theme...`);
+  //     if (themeLight) setMode('light');
+  //     else setMode('dark');
+  //   });
+  // });
+
+  function setTheme() {
+    themeLight = !themeLight;
+    if (themeLight) setMode('light');
+    else setMode('dark');
+  }
 </script>
 
 <ModeWatcher track={false} defaultMode={'dark'} />
-<Button onclick={toggleMode} variant="outline" size="icon">
+
+<!-- <Button onclick={toggleMode} variant="outline" size="icon">
   <SunIcon
     class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90" />
   <MoonIcon
     class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0" />
   <span class="sr-only">Toggle theme</span>
-</Button>
+</Button> -->
+<div class="flex items-center space-x-2">
+  <Switch
+    id="theme-controller"
+    class="!bg-input h-6 w-18"
+    bind:checked={() => themeLight, setTheme} />
+  <Label for="theme-controller">
+    <SunIcon
+      class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90" />
+    <MoonIcon
+      class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0" />
+  </Label>
+</div>
 
 {@render children()}
