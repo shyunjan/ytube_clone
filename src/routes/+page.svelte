ebb7b832da58df2d5d3ece1c5bce01e76f688a43
Shin HyunJae <shyunjan@naver.com>
2025-08-19 21:29:23 +0900

    add iconify-icon module and apply the component of it instead of Icon component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import Icon from '@iconify/svelte';
+  import 'iconify-icon';
+  // import Icon from '@iconify/svelte';
 </script>
 
 <!-- Main Layout -->
@@ -10,7 +11,12 @@
       class="flex-center hover:bg-input size-10 rounded-4xl p-2 hover:cursor-pointer"
       aria-label="menu-bars-button">
       <!-- <i class="fa-solid fa-bars text-xl"></i> -->
-      <Icon icon="iconamoon:menu-burger-horizontal-thin" class="size-6" stroke-width="1.17" />
+      <iconify-icon
+        icon="iconamoon:menu-burger-horizontal-thin"
+        width="24"
+        height="24"
+        style="stroke-width: 1.17px"></iconify-icon>
+      <!-- <Icon icon="iconamoon:menu-burger-horizontal-thin" class="size-6" stroke-width="1.17" /> -->
     </button>
     <img src="/logos/Youtube-logo-with-flat-design-edited.png" alt="logo" class="mx-2 h-8 w-30" />
   </header>
